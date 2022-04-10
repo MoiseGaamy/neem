5108b3f68a4aecb5283c1d6c211ea55e51da5ee3
Gamy <flokygamy@gmail.com>
2022-04-10 08:33:50 +0000

    struggled to add payment system

--- screens/payement/ProceedPayment.js
+++ screens/payement/ProceedPayment.js
@@ -1,28 +1,53 @@
 import { StyleSheet, Text, View,useWindowDimensions,KeyboardAvoidingView } from 'react-native'
-import React from 'react'
+import React,{useRef} from 'react'
 import { TextInput, TouchableOpacity } from 'react-native'
+import { Paystack, paystackProps } from 'react-native-paystack-webview';
+import StripeApp from './stripe/StripeApp.js';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
-const ProceedPayment = () =>
+const ProceedPayment = ({navigation}) =>
 {
      const widowWith = useWindowDimensions().height
     const padding = widowWith
+
+    const paystackWebViewRef = useRef(paystackProps.PayStackRef); 
     return (
      <KeyboardAvoidingView behavior={`${padding}`} style={{flex:1,minHeight:widowWith}}>
             
     <View style={styles.container}>
-          <View style={{flex:0.5,backgroundColor:"#fff",borderRadius:30,justifyContent:'space-evenly',alignItems:"center"}}>
-                  <Text style={{fontSize:19,fontFamily:'robo'}}>Payment</Text>
+          <View style={{flex:0.5,backgroundColor:"#fff",borderRadius:30,justifyContent:'space-around',alignItems:"center"}}>
+                  <Text style={{fontSize:19,fontFamily:'robo',textAlign:"center",}}>Payment</Text>
               <View style={{flex:0.5,justifyContent:'space-evenly'}}>
                     <View style={{justifyContent:"space-around"}}>
                         <Text style={{fontFamily:'robo'}}>Email</Text>
-                        <TouchableOpacity style={{flex:0.6,borderRadius:15,width:300,height:15,backgroundColor:'#D1D1D1'}}><TextInput placeholder='email'/></TouchableOpacity>
+                        <TouchableOpacity style={{flex:0.6,borderRadius:15,width:300,height:15,backgroundColor:'#D1D1D1',justifyContent:"center",alignItems:"center"}}><TextInput  autoCapitalize='none' keyboardType='email-address' placeholder='email'/></TouchableOpacity>
                     </View>
                     <View style={{justifyContent:"space-around"}}>
                         <Text style={{fontFamily:'robo'}}>Amount</Text>
-                        <TouchableOpacity style={{flex:0.6,borderRadius:15,width:300,backgroundColor:'#D1D1D1'}}><TextInput placeholder='enter amount'/></TouchableOpacity>
+                        <TouchableOpacity style={{flex:0.6,borderRadius:15,width:300,backgroundColor:'#D1D1D1',justifyContent:"center",alignItems:"center"}}><TextInput  placeholder='enter amount'/></TouchableOpacity>
                     </View>
               </View>
-              <TouchableOpacity style={{flex:0.1,borderRadius: 10, width: 160, backgroundColor: '#14A5A1',justifyContent:"center",alignItems:"center" }}><Text style={{color:"#fff",fontFamily:"inconsolata",fontSize:16}}>Proceed Payment</Text></TouchableOpacity>
+                   <Paystack  
+                        paystackKey="pk_test_965b964d3bda76e31fb4d254cc672ea103615cd2"
+                        amount={'25000.00'}
+                        billingEmail="paystackwebview@something.com"
+                        activityIndicatorColor="green"
+                        onCancel={(e) =>
+                        {
+                         console.log(e)
+                         navigation.navigate('drawer') 
+                        }}
+                        onSuccess={(res) =>
+                        {
+                        // handle response here
+                        }}
+                        ref={paystackWebViewRef}
+                        autoStart={false}
+                    />
+                    <TouchableOpacity onPress={()=>  paystackWebViewRef.current.startTransaction()} style={{ flex: 0.1, borderRadius: 10, width: 160, backgroundColor: '#14A5A1', justifyContent: "center", alignItems: "center" }}><Text style={{ color: "#fff", fontFamily: "inconsolata", fontSize: 16 }}>Proceed Payment</Text></TouchableOpacity>
+                    {/* <StripeProvider publishableKey='pk_test_51KmZ6wJTYLp9uVuoLj2PMClgIcKQlKLbn8sDV9atVKYPSpuI9femLRz44i3m47h83ObHXTxqnrTNry2v8a3vKDye00NVJoTnWX'>
+                       <StripeApp />
+                    </StripeProvider> */}
          </View>
     </View>
      </KeyboardAvoidingView> 
