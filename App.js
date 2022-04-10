4e6db022fa00b8d932a2e4af26b4024a0176b2ad
Gamy <flokygamy@gmail.com>
2022-04-10 08:27:31 +0000

    changed navigation emplacement

--- App.js
+++ App.js
@@ -1,34 +1,20 @@
 import 'react-native-gesture-handler';
-import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import React, { useState,useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LogoScreen from './screens/LogoScreen.js';
-import SignUp from './screens/SignUp.js';
-import SignIn from './screens/SignIn.js';
-import HomeScreen from './screens/HomeScreen.js';
-import Detail from "./screens/Detail.js"
-import MyDrawer from './screens/Drawer/MyDrawer.js';
-import Pay from "./screens/payement/Pay.js"
-import AddUnlinked from './screens/addAccounts/AddUnlinked.js';
-import Success from './screens/addAccounts/Success.js';
 import { auth } from './firebase/config.js';
-
 import { Provider } from 'react-redux';
-import store from './redux/store.js';
-
-
+import { store,persistor } from './redux/store.js';
+import AppContainer from './navigations/navigation.js';
 import useFonts from "./hooks/useFonts.js"
-import ProceedPayment from './screens/payement/ProceedPayment.js';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 export default function App()
 {
   const [IsReady, setIsready] = useState(false)
   const [currentUser,setCurrentUser]= useState(null)
-
 
   useEffect(() =>
   {
@@ -55,22 +41,10 @@ export default function App()
   const Stack = createNativeStackNavigator()
    
   return (
-      <Provider store={store}>
-      <NavigationContainer>
-          <Stack.Navigator initialRouteName="splash">
-          {!currentUser ? <>
-          <Stack.Screen name="splash" component={LogoScreen} options={{ headerShown:false}}/>
-          <Stack.Screen name="signup" component={SignUp} options={{headerShown:false}}/>
-          <Stack.Screen name="signin" component={SignIn} options={{headerShown:false}}/>
-          </> : <>
-          <Stack.Screen name="drawer" component={MyDrawer} options={{headerShown:false}}/>
-          <Stack.Screen name="feed" component={Detail} />
-          <Stack.Screen name="Proceed" component={ProceedPayment}  options={{headerShown:false}}/>
-          <Stack.Screen name="Add Unlinked Account" component={AddUnlinked} />
-          <Stack.Screen name="Add UnlinkedAccount" component={Success} />
-          </>}
-          </Stack.Navigator>
-      </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AppContainer />
+      </PersistGate>
       </Provider>
       
    
