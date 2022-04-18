bb1ac6e068a5007009c33e03c69f64ad71b7e52b
Gamy <flokygamy@gmail.com>
2022-04-18 04:46:47 +0000

    implemented goal achieved screen

---
+++ screens/GoalAchieved.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GoalAchieved = ({ item }) =>
+{
+    const { icon, name, text } = item;
+  return (
+    <View style={styles.container}>
+        <View style={{flex:1,backgroundColor:"#cfeef7",flexDirection: "row",width:380,height:130,borderRadius:20,margin:10}}>
+            <View style={{ flex: 0.3,alignItems:"center",justifyContent:"center" }}>
+                <View style={{ backgroundColor: "#fff", width: 100, height: 100, borderRadius: 100,justifyContent:"center",alignItems:"center" }}>
+                    {icon}
+                </View>
+        </View>
+            <View style={{ flex: 0.7,justifyContent:"center", alignItems:"center" }}>
+                <Text style={{ fontSize: 18 }}>{name}</Text>
+                <Text style={{ fontSize: 18, fontFamily: "inconsolata" }}>{text}</Text>
+            </View>
+            
+        </View>
+    </View>
+  )
+}
+
+export default GoalAchieved
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // backgroundColor: "#EEEEEE",
+        // padding:10
+    }
+})
