4fa8f52d0f203e633497d29186ecfb3d4c24667f
Gamy <flokygamy@gmail.com>
2022-04-18 04:51:37 +0000

    catched goals state

--- screens/TabScreens/Progress.js
+++ screens/TabScreens/Progress.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View,FlatList } from 'react-native'
+import { StyleSheet, Text, View,FlatList,Image,TouchableOpacity } from 'react-native'
 import React from 'react';
 import { Ionicons, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 import Goal from '../Goal.js';
+import { connect } from 'react-redux';
 
-const Progress = () =>
+const Progress = ({goals,navigation}) =>
 {
+ console.log(goals)
   
   const data = [
     {
@@ -33,34 +35,44 @@ const Progress = () =>
     },
   ]
   return (
-    <View style={styles.container}>
-      <View style={styles.firstContainer}>
-        <View>
-          <Text>You've Already Saved</Text>
-          <Text style={{fontSize:25}}>$30.500</Text>
+    
+        <View style={styles.container}>
+          <View style={styles.firstContainer}>
+            <View>
+              <Text>You've Already Saved</Text>
+              <Text style={{fontSize:25}}>30.500 Ghs</Text>
+            </View>
+            <View style={{ backgroundColor: "#999", width: 80, height: 60, borderRadius: 40 }}>
+              <Image source={require('../../assets/profile.png')} style={{width:80,height:60,borderRadius:40}}/>
+            </View>
         </View>
-        <View style={{backgroundColor:"#999",width:80,height:60,borderRadius:40}}></View>
-     </View>
-      <View style={styles.secondContainer}>
-         <Text style={{flex:0.3}}>Your goals</Text>
-        <View style={{flex:0.3,flexDirection:"row", justifyContent:"space-around"}}>
-          <Text>All</Text>
-         <Text>Achieved</Text>
-         </View>
-     </View>
-      <View style={styles.thirdContainer}>
-        <FlatList data={data} keyExtractor={(item) => item.id} renderItem={({ item }) => (<Goal item={item}/>)}/>
-     </View>
-    </View>
+          <View style={styles.secondContainer}>
+            <Text style={{flex:0.3}}>Your goals</Text>
+            <View style={{flex:0.3,flexDirection:"row", justifyContent:"space-around"}}>
+              <Text>All</Text>
+               <TouchableOpacity onPress={()=> navigation.navigate('achieved')}><Text>Achieved</Text></TouchableOpacity>
+            </View>
+        </View>
+          <View style={styles.thirdContainer}>
+            <FlatList data={data} keyExtractor={(item) => item.id} renderItem={({ item }) => (<Goal item={item}/>)}/>
+        </View>
+        </View>
   )
 }
 
-export default Progress
+const mapStateToProps = (state) =>
+{
+  return {
+    goals:state.goals.goals
+  }
+}
+
+export default connect(mapStateToProps)(Progress)
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // backgroundColor:"green"
+         backgroundColor:"#cfeef7",
   },
   firstContainer: {
     flex: 0.2,
