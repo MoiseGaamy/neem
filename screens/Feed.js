bba548983acf4c85f4ceb65a64573c1145b8e535
Tony <toneyobaze@gmail.com>
2022-04-20 11:55:14 +0000

    changed headline

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,39 +1,38 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
 
-const Feed = ({item}) =>
-{
-    const { image, headLine, para, date:newDate ,id} = item;
-    const date = new Date().toDateString()
+const Feed = ({ item }) => {
+  const { image, headLine, para, date: newDate, id } = item;
+  const date = new Date().toDateString();
   return (
     <View key={id} style={styles.container}>
-          <View style={styles.detailcontainer}>
-              <View>
-                  <Image style={{width:370,height:150}} source={image}/>
-              </View>
-         <Text style={{fontSize:18,lineHeight:30}}>{headLine}</Text>
-         <Text>{para}</Text>
-         <Text style={{fontSize:17}}>{newDate}</Text>
-     </View>
+      <View style={styles.detailcontainer}>
+        <View>
+          <Image style={{ width: 350, height: 150 }} source={image} />
+        </View>
+        <Text style={{ fontSize: 18, lineHeight: 30 }}>{headLine}</Text>
+        <Text>{para}</Text>
+        <Text style={{ fontSize: 17 }}>{newDate}</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding:10
-    },
-    detailcontainer: {
-        flex: 1,
-        padding: 10,
-        borderWidth: 1,
-        borderColor: "#fff",
-        backgroundColor:"#cfeef7",
-        borderRadius: 8,
-        justifyContent:"space-evenly"
-        // height:"80%"
-    }
-})
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  detailcontainer: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#fff",
+    backgroundColor: "#cfeef7",
+    borderRadius: 8,
+    justifyContent: "space-evenly",
+    // height:"80%"
+  },
+});
