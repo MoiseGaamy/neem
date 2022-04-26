5290f6f3cbcb3656a42ad21806a9901fc78710f7
Gamy <flokygamy@gmail.com>
2022-04-26 16:55:37 +0000

    changed userActions

--- redux/actions/userActions.js
+++ redux/actions/userActions.js
@@ -1,35 +1,39 @@
 import { fireDB } from "../../firebase/config.js";
+import axios from 'axios'
 //action to create new Goal
 export const addGoal = (newGoal) => {
-    return (dispatch) =>
+    return async (dispatch, state) =>
     {
-        console.log("jfjfjfjfjfj", newGoal);
-            // fireDB
-            // .collection("goals")
-            // .add(newGoal)
-            // .then((goal) => {console.log('this is it', goal)})
-            // .catch((error)=>{
-            //     console.log(error.message)
-            // });
-    }
+        try
+    {
+        const {data} = await axios.post("https://secure-lake-72191.herokuapp.com/plan", newGoal)
+        dispatch(getAllGoals());
+    } catch (e)
+    {
+        console.log(e);
+   }
+   }
 };
 
 export const getAllGoals = () => {
-	return (dispatch,state) => {
-		    fireDB
-			.collection("goals")
-			.onSnapshot(
-				(snapshot) => {
-					let allGoals = [];
-                    snapshot.forEach((doc) =>
-                    {
-                        console.log(doc.data());
-						allGoals.push(doc.data());
-					});
-                    console.log(allGoals); 
-                     dispatch({ type: "ALL_GOALS", payload: allGoals }); 
-				},
-            )
-	};
+    return async (dispatch, state) =>
+    {
+        const { data } = await axios.get("https://secure-lake-72191.herokuapp.com/plan")
+        dispatch({ type: "ALL_GOALS", payload: data });
+    }
+		    // fireDB
+			// .collection("goals")
+			// .onSnapshot(
+			// 	(snapshot) => {
+			// 		let allGoals = [];
+            //         snapshot.forEach((doc) =>
+            //         {
+            //             console.log(doc.data());
+			// 			allGoals.push(doc.data());
+			// 		});
+            //         console.log(allGoals); 
+            //          dispatch({ type: "ALL_GOALS", payload: allGoals }); 
+			// 	},
+            // )
 };
 
