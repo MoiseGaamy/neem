20269f0b7053537fb5f9360e97c0cec5327ca900
Gamy <flokygamy@gmail.com>
2022-04-26 16:56:06 +0000

    changed the state

--- screens/Goal.js
+++ screens/Goal.js
@@ -1,35 +1,54 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import * as Progress from 'react-native-progress';
+import { EvilIcons, MaterialCommunityIcons,Ionicons,MaterialIcons,Foundation} from "@expo/vector-icons";
 
 const Goal = ({ item }) =>
 {
-    const { id, icon, name, targetAmount, selectedReminder,amountForContribution, monthLeft, startPrice, endPrice } = item
+    const { id, purpose: {iconName, iconType}, name, targetAmount, selectedReminder,amountForContribution, monthLeft, startPrice, endPrice } = item
   
     let contribution;
-    let target = Number(targetAmount);
-    let contribu = Number(amountForContribution);
-    if (selectedReminder === 'mth')
+    
+    if (selectedReminder === 'month')
     {
-        contribution = (contribu * 30) / target
-        console.log(contribution)
-    } else if (selectedReminder === 'wk')
+        contribution = (amountForContribution * 30) / targetAmount
+    } else if (selectedReminder === 'week')
     {
-        contribution = ( contribu * 7) / target
+        contribution = ( amountForContribution * 7) / targetAmount
     } else
     {
-        contribution = contribu /target
+        contribution = amountForContribution /targetAmount
     }
 
+    const icon = () =>
+    {
+        switch (iconType)
+        {
+            case "EvilIcons":
+                return <EvilIcons name={iconName} size={24} color='#A1E3D8' />
+            
+            case "Ionicons":
+                return <Ionicons name={iconName} size={24} color='#A1E3D8' />
+            
+            case "MaterialIcons":
+                return <MaterialIcons name={iconName} size={24} color='#A1E3D8' />
+            
+            case "Foundation":
+                return <Foundation name={iconName} size={24} color='#A1E3D8' />
+
+            case "MaterialCommunityIcons":
+                return <MaterialCommunityIcons name={iconName} size={24} color='#A1E3D8' />
+        }
+    }
    
   return (
     <View key={id} style={styles.container}>
         <View style={{flex:1,backgroundColor:"#2dbdba",borderRadius:20}}>
               <View style={{flex:1,flexDirection:"row",justifyContent:"space-between",height:100}}>    
             <View style={{flex:0.3,alignItems:"center",justifyContent:"center"}}>
-                 <View style={{backgroundColor:"grey",width:60,height:60,borderRadius:15,alignItems:"center",justifyContent:"center"}}>
-                <Text>{icon}</Text>
-            </View> 
+                 <View style={{backgroundColor:"#069A8E",width:60,height:60,borderRadius:15,alignItems:"center",justifyContent:"center"}}>
+                {icon()}
+                      </View> 
             </View>
             <View style={{flex:0.7,alignItems:"flex-start",justifyContent:"center"}}>
                 <Text>{name}</Text>
@@ -39,12 +58,12 @@ const Goal = ({ item }) =>
          <View style={{flex:1,paddingLeft:30,height:50}}>
             
         <View style={{backgroundColor:"#203239",width:"100%",height:"15%",borderRadius:7,justifyContent:"center"}}>
-                      <Progress.Bar progress={contribu} width={200} />
+                      <Progress.Bar progress={contribution} width={200} />
                       {/* <View style={{backgroundColor:"#E6D5B8",width:"60%",height:"20%",borderRadius:5}}></View> */}
              </View>
              <View style={{flex:1,flexDirection:"row",justifyContent:"space-between",alignItems:"center"}}>
                       <Text>{targetAmount}</Text>
-                      <Text>{amountForContribution}</Text>
+                      <Text>{targetAmount - amountForContribution}</Text>
              </View>
              
          </View>
